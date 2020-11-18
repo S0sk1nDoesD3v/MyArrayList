92d6b479fcb02311fa2d96e13bcef78713425eca
Mike Soskin <62444444+S0sk1nDoesD3v@users.noreply.github.com>
2020-11-17 20:07:31 -0500

    Update ArrayDriver.java

--- ArrayDriver.java
+++ ArrayDriver.java
@@ -5,17 +5,16 @@ public class ArrayDriver {
         test.add("Michael");
         test.add("Remove");
         test.add("Kingdom");
-        //System.out.println(test.contains("Hello"));
-        //System.out.println(test.capacity());
-        //System.out.println(test.clear());
-       // System.out.println(test.indexOf("Remove"));
+        System.out.println(test.contains("Hello"));
+        System.out.println(test.capacity());
+        System.out.println(test.clear());
+        System.out.println(test.indexOf("Remove"));
         System.out.println(test.indexOf("Michael"));
-        //System.out.println(test.isEmpty());
+        System.out.println(test.isEmpty());
         System.out.println(test.remove("Michael"));
-        //System.out.println(test.size());
-
+        System.out.println(test.size());
         System.out.println(test.remove(2));
-        //System.out.println(test.get(0));
+        System.out.println(test.get(0));
 
 
 
